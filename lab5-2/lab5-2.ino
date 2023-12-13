4c0ff6c2a12efebfaa432fab467ca01bf0f585c4
Panthakit totid <koontyza007@gmail.com>
2023-12-13 11:29:16 +0700

    Update lab5-2.ino

--- lab5-2/lab5-2.ino
+++ lab5-2/lab5-2.ino
@@ -1,9 +1,19 @@
+#include "USB.h"
+#include "USBHIDKeyboard.h"
+USBHIDKeyboard Keyborad;
+
 void setup() {
   // put your setup code here, to run once:
+  Keyborad.begin();
+  USB.begin();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Keyborad.press(KEY_BACKSPAC);
+  delay(100);
+  Keyborad.releaseAll();
+  delay(1000);
 
 }
