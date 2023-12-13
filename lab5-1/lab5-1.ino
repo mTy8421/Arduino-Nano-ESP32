0e6c93fd1f5fd4c578eeb8f4867c5172a39f532a
Panthakit totid <koontyza007@gmail.com>
2023-12-13 11:23:46 +0700

    Update lab5-1.ino

--- lab5-1/lab5-1.ino
+++ lab5-1/lab5-1.ino
@@ -1,12 +1,19 @@
 #include "USB.h"
 #include "USBHIDKeyboard.h"
+USBHIDKeyboard Keyborad;
 
 void setup() {
   // put your setup code here, to run once:
+  Keyborad.begin();
+  USB.begin();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Keyborad.press('s');
+  delay(100);
+  Keyborad.releaseAll();
+  delay(1000);
 
 }
