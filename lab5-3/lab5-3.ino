3a437e1da73194a689896a4965cdc994cbf70f13
Panthakit totid <koontyza007@gmail.com>
2023-12-13 11:35:40 +0700

    Update lab5-3.ino

--- lab5-3/lab5-3.ino
+++ lab5-3/lab5-3.ino
@@ -6,14 +6,18 @@ void setup() {
   // put your setup code here, to run once:
   Keyborad.begin();
   USB.begin();
+  pinMode(17, INPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Keyborad.press(KEY_BACKSPACE);
-  delay(100);
-  Keyborad.releaseAll();
-  delay(1000);
+  int kb_1 = digitalRead(17);
+  if(kb_1 == 1){
+    Keyborad.press(KEY_BACKSPACE);
+    delay(100);
+    Keyborad.releaseAll();
+    delay(1000);
+  }
 
 }
